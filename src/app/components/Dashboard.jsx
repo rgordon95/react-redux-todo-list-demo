89d8110856a8db3c6e17197a62d88bd48a3b3c0c
Richard Gordon <richgordon95@gmail.com>
2019-04-04 12:51:44 -0400

    update TaskList to show name as title

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { ConnectedTaskList } from './TaskList';
 
 export const Dashboard = ({groups}) => (
   <div>
     <h2>Dashboard</h2>
-    {groups.map(group => (<div> {group.name} </div>))}
+    {groups.map(group => (
+      <ConnectedTaskList id={group.id} name={group.name} />
+    ))}
   </div>
-)
+);
 
 function mapStateToProps (state) {
   return {
