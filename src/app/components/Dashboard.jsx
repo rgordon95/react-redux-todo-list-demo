a52c8badd578ccdb3a82979b7f049898eba4ba62
Richard Gordon <richgordon95@gmail.com>
2019-04-04 12:43:46 -0400

    map groups into dashboard

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 export const Dashboard = ({groups}) => (
   <div>
     <h2>Dashboard</h2>
+    {groups.map(group => (<div> {group.name} </div>))}
   </div>
 )
 
