176b7f12ecc74ad1624167d34cd61adfb6b77086
Richard Gordon <richgordon95@gmail.com>
2019-04-04 12:54:16 -0400

    add Router and History dependencies

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard'
+import { Router, Router } from 'react-router-dom';
 
 export const Main = () => (
-  <Provider store={store}>
-    <div>
-      <ConnectedDashboard />
-    </div>
-  </Provider>
+  <Router history={}>
+    <Provider store={store}>
+      <div>
+        <ConnectedDashboard />
+      </div>
+    </Provider>
+  </Router>
 )
