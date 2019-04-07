a9bcad3e1397aebdd68616b78ce1c827d2f395a8
Richard Gordon <richgordon95@gmail.com>
2019-04-07 15:11:18 -0400

    add info and styling to task detail

--- src/app/components/TaskDetail.jsx
+++ src/app/components/TaskDetail.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 
 const TaskDetail = ({
   id,
   comments,
   task,
-  isComplete
+  isComplete,
+  groups
 }) => (
   <div>
-    Task Detail
+    <div>
+      <input value={task.name}> </input>
+    </div>
+    <button> Complete / Reopen Task </button>
+    <div>
+      <select>
+        {groups.map(group => (
+          <option key={group.id} value={group.id}>{group.name}</option>
+        ))}
+      </select>
+    </div>
+    <div>
+      <Link to="/dashboard">
+    </div>
+      <div>
+        <button>Done</button>
+      </div>
+    </Link>
   </div>
 );
 
-const mapStateToProps = state => state;
+const mapStateToProps = (state, ownProps) => {
+let id = ownProps.match.params.id;
+let task = state.tasks.find(task => task.id === id);
+let groups = state.groups;
+
+return {
+  id,
+  task,
+  groups,
+  isComplete:task.isComplete
+  }
+}
 
 export const ConnectedTaskDetail = connect(mapStateToProps) (TaskDetail);
