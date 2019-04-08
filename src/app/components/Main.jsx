c4c95917ae9ceb55e8fa8a9f804baee344a48796
Richard Gordon <richgordon95@gmail.com>
2019-04-07 20:39:16 -0400

    begin authentication with routeGuarding and redirect

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,25 +2,43 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard'
+import { ConnectedLogin } from './Login';
 import { Route, Router } from 'react-router-dom';
+import { Redirect } from 'react-router';
 import { history } from '../store/history';
 import { ConnectedNavigation } from './Navigation';
 import { ConnectedTaskDetail } from './TaskDetail';
+
+// console.info("Route guard:", match);
+// if (!store.getState().session || !store.getState().session.authenticated) {
+//   return <Redirect to="/" />;
+// } else {
+
+const RouteGuard = Component => ({ match }) => {
+    if (!store.getState().session.authenticated) {
+      return <Redirect to="/" />
+    } {
+      return <Component match={match} />
+
+    }
+
+}
 
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
       <div>
         <ConnectedNavigation />
+        <Route exact path ="/" component={ConnectedLogin} />
         <Route
           exact
           path="/dashboard"
-          render={ () => (<ConnectedDashboard />)}
+          render={RouteGuard(ConnectedDashboard)}
         />
         <Route
           exact
           path="/task/:id"
-          render={ ({ match }) => (<ConnectedTaskDetail match={ match }/>)}
+          render={RouteGuard(ConnectedTaskDetail)}
         />
       </div>
     </Provider>
