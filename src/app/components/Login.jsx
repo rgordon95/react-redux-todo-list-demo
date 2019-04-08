42273fd2cf6e313a08aa3c3cc4397b5e178695c0
Richard Gordon <richgordon95@gmail.com>
2019-04-08 13:19:28 -0400

    user side rendering of login authenticity

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import * as mutations from '../store/mutations';
 import { connect } from 'react-redux';
 
-const LoginComponent = ({ authenticateUser }) => {
+const LoginComponent = ({ authenticateUser, authenticated }) => {
   return <div>
     <h2>Please login</h2>
     <form onSubmit={authenticateUser}>
       <input type="text" placeholder="username" name="username" defaultValue="Dev" />
       <input type="password" placeholder="password" name="password" defaultValue="" />
+      {authenticated === mutations.NOT_AUTHENTICATED ? <p> Login incorrect </p> : null}
       <button type="submit">Login</button>
     </form>
   </div>
 };
 
-const mapStateToProps = state => state;
+const mapStateToProps = ({session})=> ({
+  authenticated: session.authenticated
+});
 
 const mapDispatchToProps = (dispatch) => ({
   authenticateUser(e) {
