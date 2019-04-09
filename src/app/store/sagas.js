2f5b5e17ae5ecad3d1da04e0ebf092a2825a7a37
Richard Gordon <richgordon95@gmail.com>
2019-04-09 14:21:27 -0400

    link final mutations

--- src/app/store/sagas.js
+++ src/app/store/sagas.js
@@ -3,6 +3,7 @@ import uuid from 'uuid';
 import axios from 'axios';
 
 import * as mutations from './mutations';
+import { history } from './history';
 
 const url = "http://localhost:7069";
 
@@ -51,6 +52,10 @@ export function* userAuthenticationSaga() {
         throw new Error();
         console.log("Authenticated!", data);
 
+        yield put(mutations.setState(data.state));
+        yield put (mutations.processAuthenticateUser(mutations.AUTHENTICATED));
+
+        history.push('/dashboard');
       }
     } catch (e) {
       console.log(`can't authenticate`);
