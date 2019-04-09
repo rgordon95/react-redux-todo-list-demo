6217da4f7751d5fc9cd58d88e5502c209912de8e
Richard Gordon <richgordon95@gmail.com>
2019-04-09 16:23:40 -0400

    IIF

--- src/server/initialize-db.js
+++ src/server/initialize-db.js
@@ -1,7 +1,7 @@
 import { defaultState } from './defaultState';
 import { connectDB } from './connect-db';
 
-async function initializeDB() {
+(async function initializeDB() {
   let db = await connectDB();
   let user = await db.collection("users".findOne)({id:"U1"});
   if (!user) {
@@ -12,6 +12,4 @@ async function initializeDB() {
     }
   }
 
-};
-
-initializeDB();
+})();
